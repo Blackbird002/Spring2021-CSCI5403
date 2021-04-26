4fe9619c05112a314fdcb1fd0131f4ed088581da
Riad Shash <riadshash@yahoo.com>
2021-04-25 23:00:14 -0600

    Simplified shift lookup, comments

--- md5.py
+++ md5.py
@@ -18,10 +18,10 @@ import argparse
 INT_BITS = 32
  
 ################################################################
-# Table of 64 elements where SV[i] = abs(sin(i+1)) * 2^32
+# Table of 64 elements where K[i] = abs(sin(i+1)) * 2^32
 # Pseudo-random numbers that have desirable properties
 ################################################################
-SV = [  0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 
+K = [  0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 
         0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af,
         0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 
         0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 
@@ -36,32 +36,21 @@ SV = [  0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf,
         0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391              ]
 
 ################################################################
-# Constants for MD5Transform routine (shift amounts)
+# Constants for MD5Transform routine (bit shift amounts)
+# These repeat 4 times within a round
 ################################################################
 
 # Round 1
-S11 = 7
-S12 = 12
-S13 = 17
-S14 = 22
+S1 = [7,12,17,22]
 
 # Round 2
-S21 = 5
-S22 = 9
-S23 = 14
-S24 = 20
+S2 = [5,9,14,20]
 
 # Round 3
-S31 = 4
-S32 = 11
-S33 = 16
-S34 = 23
+S3 = [4,11,16,23]
 
 # Round 4
-S41 = 6
-S42 = 10
-S43 = 15
-S44 = 21
+S4 = [6,10,15,21]
  
 # Circular bit-shift helper function
 def leftCircularShift(k ,bits):
@@ -81,15 +70,20 @@ def blockDivide(block, chunks):
 ################################################################
 # Basic Functions F, G, H, I
 ################################################################
+
+# For 0 <= step# <= 15
 def F(X,Y,Z):
     return((X & Y)|((~X) & Z))
  
+# For 16 <= step# <= 31
 def G(X,Y,Z):
     return((X & Z)|(Y &(~Z)))
  
+# For 32 <= step# <= 47
 def H(X,Y,Z):
     return(X^Y^Z)
- 
+
+# For 48 <= step# <= 63
 def I(X,Y,Z):
     return(Y^(X|(~Z)))
 
@@ -189,76 +183,76 @@ def md5sum(msg):
         # For each block we perform 4 rounds where each round has 16 operations (Total of 64). 
 
         #Round 1
-        a = FF( a,b,c,d, M[0], S11, SV[0] )           #1
-        d = FF( d,a,b,c, M[1], S12, SV[1] )           #2
-        c = FF( c,d,a,b, M[2], S13, SV[2] )           #3
-        b = FF( b,c,d,a, M[3], S14, SV[3] )           #4
-        a = FF( a,b,c,d, M[4], S11, SV[4] )           #5
-        d = FF( d,a,b,c, M[5], S12, SV[5] )           #6
-        c = FF( c,d,a,b, M[6], S13, SV[6] )           #7
-        b = FF( b,c,d,a, M[7], S14, SV[7] )           #8
-        a = FF( a,b,c,d, M[8], S11, SV[8] )           #9
-        d = FF( d,a,b,c, M[9], S12, SV[9] )           #10
-        c = FF( c,d,a,b, M[10], S13, SV[10] )         #11
-        b = FF( b,c,d,a, M[11], S14, SV[11] )         #12
-        a = FF( a,b,c,d, M[12], S11, SV[12] )         #13
-        d = FF( d,a,b,c, M[13], S12, SV[13] )         #14
-        c = FF( c,d,a,b, M[14], S13, SV[14] )         #15
-        b = FF( b,c,d,a, M[15], S14, SV[15] )         #16
+        a = FF( a,b,c,d, M[0], S1[0], K[0] )           #1
+        d = FF( d,a,b,c, M[1], S1[1], K[1] )           #2
+        c = FF( c,d,a,b, M[2], S1[2], K[2] )           #3
+        b = FF( b,c,d,a, M[3], S1[3], K[3] )           #4
+        a = FF( a,b,c,d, M[4], S1[0], K[4] )           #5
+        d = FF( d,a,b,c, M[5], S1[1], K[5] )           #6
+        c = FF( c,d,a,b, M[6], S1[2], K[6] )           #7
+        b = FF( b,c,d,a, M[7], S1[3], K[7] )           #8
+        a = FF( a,b,c,d, M[8], S1[0], K[8] )           #9
+        d = FF( d,a,b,c, M[9], S1[1], K[9] )           #10
+        c = FF( c,d,a,b, M[10], S1[2], K[10] )         #11
+        b = FF( b,c,d,a, M[11], S1[3], K[11] )         #12
+        a = FF( a,b,c,d, M[12], S1[0], K[12] )         #13
+        d = FF( d,a,b,c, M[13], S1[1], K[13] )         #14
+        c = FF( c,d,a,b, M[14], S1[2], K[14] )         #15
+        b = FF( b,c,d,a, M[15], S1[3], K[15] )         #16
 
         # Round 2
-        a = GG( a,b,c,d, M[1], S21, SV[16] )          #17
-        d = GG( d,a,b,c, M[6], S22, SV[17] )          #18
-        c = GG( c,d,a,b, M[11], S23, SV[18] )         #19
-        b = GG( b,c,d,a, M[0], S24, SV[19] )          #20
-        a = GG( a,b,c,d, M[5], S21, SV[20] )          #21
-        d = GG( d,a,b,c, M[10], S22, SV[21] )         #22
-        c = GG( c,d,a,b, M[15], S23, SV[22] )         #23
-        b = GG( b,c,d,a, M[4], S24, SV[23] )          #24
-        a = GG( a,b,c,d, M[9], S21, SV[24] )          #25
-        d = GG( d,a,b,c, M[14], S22, SV[25] )         #26
-        c = GG( c,d,a,b, M[3], S23, SV[26] )          #27
-        b = GG( b,c,d,a, M[8], S24, SV[27] )          #28
-        a = GG( a,b,c,d, M[13], S21, SV[28] )         #29
-        d = GG( d,a,b,c, M[2], S22, SV[29] )          #30
-        c = GG( c,d,a,b, M[7], S23, SV[30] )          #31
-        b = GG( b,c,d,a, M[12], S24, SV[31] )         #32
+        a = GG( a,b,c,d, M[1], S2[0], K[16] )          #17
+        d = GG( d,a,b,c, M[6], S2[1], K[17] )          #18
+        c = GG( c,d,a,b, M[11], S2[2], K[18] )         #19
+        b = GG( b,c,d,a, M[0], S2[3], K[19] )          #20
+        a = GG( a,b,c,d, M[5], S2[0], K[20] )          #21
+        d = GG( d,a,b,c, M[10], S2[1], K[21] )         #22
+        c = GG( c,d,a,b, M[15], S2[2], K[22] )         #23
+        b = GG( b,c,d,a, M[4], S2[3], K[23] )          #24
+        a = GG( a,b,c,d, M[9], S2[0], K[24] )          #25
+        d = GG( d,a,b,c, M[14], S2[1], K[25] )         #26
+        c = GG( c,d,a,b, M[3], S2[2], K[26] )          #27
+        b = GG( b,c,d,a, M[8], S2[3], K[27] )          #28
+        a = GG( a,b,c,d, M[13], S2[0], K[28] )         #29
+        d = GG( d,a,b,c, M[2], S2[1], K[29] )          #30
+        c = GG( c,d,a,b, M[7], S2[2], K[30] )          #31
+        b = GG( b,c,d,a, M[12], S2[3], K[31] )         #32
 
         # Round 3
-        a = HH( a,b,c,d, M[5], S31, SV[32] )          #33
-        d = HH( d,a,b,c, M[8], S32, SV[33] )          #34
-        c = HH( c,d,a,b, M[11], S33, SV[34] )         #35
-        b = HH( b,c,d,a, M[14], S34, SV[35] )         #36
-        a = HH( a,b,c,d, M[1], S31, SV[36] )          #37
-        d = HH( d,a,b,c, M[4], S32, SV[37] )          #38
-        c = HH( c,d,a,b, M[7], S33, SV[38] )          #39
-        b = HH( b,c,d,a, M[10], S34, SV[39] )         #40
-        a = HH( a,b,c,d, M[13], S31, SV[40] )         #41
-        d = HH( d,a,b,c, M[0], S32, SV[41] )          #42
-        c = HH( c,d,a,b, M[3], S33, SV[42] )          #43
-        b = HH( b,c,d,a, M[6], S34, SV[43] )          #44
-        a = HH( a,b,c,d, M[9], S31, SV[44] )          #45
-        d = HH( d,a,b,c, M[12], S32, SV[45] )         #46
-        c = HH( c,d,a,b, M[15], S33, SV[46] )         #47
-        b = HH( b,c,d,a, M[2], S34, SV[47] )          #48
+        a = HH( a,b,c,d, M[5], S3[0], K[32] )          #33
+        d = HH( d,a,b,c, M[8], S3[1], K[33] )          #34
+        c = HH( c,d,a,b, M[11], S3[2], K[34] )         #35
+        b = HH( b,c,d,a, M[14], S3[3], K[35] )         #36
+        a = HH( a,b,c,d, M[1], S3[0], K[36] )          #37
+        d = HH( d,a,b,c, M[4], S3[1], K[37] )          #38
+        c = HH( c,d,a,b, M[7], S3[2], K[38] )          #39
+        b = HH( b,c,d,a, M[10], S3[3], K[39] )         #40
+        a = HH( a,b,c,d, M[13], S3[0], K[40] )         #41
+        d = HH( d,a,b,c, M[0], S3[1], K[41] )          #42
+        c = HH( c,d,a,b, M[3], S3[2], K[42] )          #43
+        b = HH( b,c,d,a, M[6], S3[3], K[43] )          #44
+        a = HH( a,b,c,d, M[9], S3[0], K[44] )          #45
+        d = HH( d,a,b,c, M[12], S3[1], K[45] )         #46
+        c = HH( c,d,a,b, M[15], S3[2], K[46] )         #47
+        b = HH( b,c,d,a, M[2], S3[3], K[47] )          #48
 
         # Round 4
-        a = II( a,b,c,d, M[0], S41, SV[48] )          #49
-        d = II( d,a,b,c, M[7], S42, SV[49] )          #50
-        c = II( c,d,a,b, M[14], S43, SV[50] )         #51
-        b = II( b,c,d,a, M[5], S44, SV[51] )          #52
-        a = II( a,b,c,d, M[12], S41, SV[52] )         #53
-        d = II( d,a,b,c, M[3], S42, SV[53] )          #54
-        c = II( c,d,a,b, M[10], S43, SV[54] )         #55
-        b = II( b,c,d,a, M[1], S44, SV[55] )          #56
-        a = II( a,b,c,d, M[8], S41, SV[56] )          #57
-        d = II( d,a,b,c, M[15], S42, SV[57] )         #58
-        c = II( c,d,a,b, M[6], S43, SV[58] )          #59
-        b = II( b,c,d,a, M[13], S44, SV[59] )         #60
-        a = II( a,b,c,d, M[4], S41, SV[60] )          #61
-        d = II( d,a,b,c, M[11], S42, SV[61] )         #62
-        c = II( c,d,a,b, M[2], S43, SV[62] )          #63
-        b = II( b,c,d,a, M[9], S44, SV[63] )          #64
+        a = II( a,b,c,d, M[0], S4[0], K[48] )          #49
+        d = II( d,a,b,c, M[7], S4[1], K[49] )          #50
+        c = II( c,d,a,b, M[14], S4[2], K[50] )         #51
+        b = II( b,c,d,a, M[5], S4[3], K[51] )          #52
+        a = II( a,b,c,d, M[12], S4[0], K[52] )         #53
+        d = II( d,a,b,c, M[3], S4[1], K[53] )          #54
+        c = II( c,d,a,b, M[10], S4[2], K[54] )         #55
+        b = II( b,c,d,a, M[1], S4[3], K[55] )          #56
+        a = II( a,b,c,d, M[8], S4[0], K[56] )          #57
+        d = II( d,a,b,c, M[15], S4[1], K[57] )         #58
+        c = II( c,d,a,b, M[6], S4[2], K[58] )          #59
+        b = II( b,c,d,a, M[13], S4[3], K[59] )         #60
+        a = II( a,b,c,d, M[4], S4[0], K[60] )          #61
+        d = II( d,a,b,c, M[11], S4[1], K[61] )         #62
+        c = II( c,d,a,b, M[2], S4[2], K[62] )          #63
+        b = II( b,c,d,a, M[9], S4[3], K[63] )          #64
 
         # Add this hash to the result
         A = (A + a) % (2**32)
